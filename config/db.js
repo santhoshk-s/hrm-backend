438d3e4b50f50691135386ee411b7ebd343a2782
sarathirangasamy <sarathirangasamy@gmail.com>
2024-12-19 16:45:58 +0530

    HRM env file changes

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.DATABASE_URL);
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
